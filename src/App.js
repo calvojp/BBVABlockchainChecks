03a12f3f212288ca0e8f6c54332269cf495a3f00
Ramiro Peidro <rpeidro@fi.uba.ar>
2023-04-24 11:44:32 -0300

    total de cheques funcionando

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { NavBar } from './components/NavBar/NavBar';
+import { NFTCheque} from './components/NFTCheque/NFTCheque.js';
+import './components/NFTCheque/NFTCheque.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <NavBar
+          logo="https://www.bbva.com/wp-content/uploads/2019/04/Logo-BBVA.jpg"
+          link1="Inicio"
+          link2="Servicios"
+          link3="Contacto"
+          ruta1="/"
+          ruta2="/services"
+          ruta3="/contact"
+        />
+        <Routes>
+          <Route path="/" element={<NFTCheque />} />
+          {/* Agrega más rutas aquí si es necesario */}
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,40 @@
+// NavBar.js
+import React, { useState } from 'react';
+import { IoMdClose, IoMdMenu } from "react-icons/io";
+import { Link, NavLink } from 'react-router-dom';
+import './NavBar.scss';
+
+export const NavBar = ({ logo, link1, link2, link3, ruta1, ruta2, ruta3 }) => {
+    const [menu, setMenu] = useState(false);
+
+    const handleMenuOnClick = () => {
+        setMenu(!menu);
+    };
+
+    return (
+        <header>
+            <div className="logo">
+                <Link to="/" className="nav-link">
+                    <img className="img-fluid" src={logo} alt="logo" />
+                </Link>
+            </div>
+
+            <nav className={menu ? "nav show" : "nav"} id="nav-menu">
+                <IoMdClose id="header-close" onClick={handleMenuOnClick} />
+
+                <ul className="nav-list">
+                    <li className="nav-item">
+                        <NavLink exact to={ruta1} className="nav-link" activeClassName={'activeLink'}>{link1}</NavLink>
+                    </li>
+                    <li className="nav-item">
+                        <NavLink exact to={ruta2} className="nav-link" activeClassName={'activeLink'}>{link2}</NavLink>
+                    </li>
+                    <li className="nav-item">
+                        <NavLink exact to={ruta3} className="nav-link" activeClassName={'activeLink'}>{link3}</NavLink>
+                    </li>
+                </ul>
+            </nav>
+            <IoMdMenu id="header-toggle" onClick={handleMenuOnClick} />
+        </header>
+    );
+};
